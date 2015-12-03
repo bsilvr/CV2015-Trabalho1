0f7e8f0748084227caca74ff5361383f706cfd60
Bruno Silva <brunomiguelsilva@ua.pt>
2015-12-03 18:56:45 +0000

    cleaned up code, fixed bug with pause

--- js/cv_webgl.js
+++ js/cv_webgl.js
@@ -1,440 +1,239 @@
 //////////////////////////////////////////////////////////////////////////////
 //
+//	Universidade de Aveiro - 2015
 //
+//	Computação Visual - Trabalho 1 WebGL
 //
-//
-//
-//
+//	Bruno Silva - 68535
+//	Bernardo Ferreira - 67413
 //
 //////////////////////////////////////////////////////////////////////////////
 
-////////////////////////////////////////////////////////////////////////////
-
 // Stats Variables
 var iterations = 0;
+var swaps = 0;
 
-
-//----------------------------------------------------------------------------
-//
 // Global Variables
-//
 var initArray = [];
-
 var randomArray = [6, 11, 15, 1, 4, 9, 10, 14, 0, 5, 17, 13, 8, 7, 2, 16, 3, 18, 12];
-
 var bluePos = -1;
-
 var ongoing = false;
-
+var paused = false;
 var delay = 200;
-
 var aux = 0;
-
 var mim = 0;
-
 var sortedVertices = null;
+var vertexPerCollumn = 36*3;
+var lastTime = 0;
+var intervalID;
+var index;
 
-var gl = null; // WebGL context
-
+// WebGL Variables
+var gl = null;
 var shaderProgram = null;
-
 var triangleVertexPositionBuffer = null;
-
 var triangleVertexColorBuffer = null;
 
-// The global transformation parameters
-
 // The translation vector
-
 var tx = 0.0;
-
 var ty = 0.0;
-
 var tz = 0.0;
 
 // The rotation angles in degrees
-
 var angleXX = 0.0;
-
 var angleYY = 0.0;
-
 var angleZZ = 0.0;
 
 // The scaling factors
-
 var sx = 0.9;
-
 var sy = 0.9;
-
 var sz = 0.9;
 
-// NEW - Animation controls
-
+// Animation controls
 var rotationXX_ON = 1;
-
 var rotationXX_DIR = 1;
-
 var rotationXX_SPEED = 1;
-
 var rotationYY_ON = 1;
-
 var rotationYY_DIR = 1;
-
 var rotationYY_SPEED = 1;
-
 var rotationZZ_ON = 1;
-
 var rotationZZ_DIR = 1;
-
 var rotationZZ_SPEED = 1;
 
-// To allow choosing the way of drawing the model triangles
+var mouseDown = false;
+var lastMouseX = null;
+var lastMouseY = null;
 
-var primitiveType = null;
-
-// To allow choosing the projection type
-
-var projectionType = 0;
-
-// For storing the vertices defining the triangles
-
-//----------------------------------------------------------------------------
-//
-// The WebGL code
-//
-
-//----------------------------------------------------------------------------
-//
-//  Rendering
-//
-
-// Handling the Vertex and the Color Buffers
-
-function initBuffers() {
-
-	// Coordinates
-
+function initBuffers(){
 	gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 	triangleVertexPositionBuffer.itemSize = 3;
 	triangleVertexPositionBuffer.numItems = vertices.length / 3;
 
-	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute,
-			triangleVertexPositionBuffer.itemSize,
-			gl.FLOAT, false, 0, 0);
+	gl.vertexAttribPointer( shaderProgram.vertexPositionAttribute,
+							triangleVertexPositionBuffer.itemSize,
+							gl.FLOAT, false, 0, 0);
 
 	gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexColorBuffer);
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
 	triangleVertexColorBuffer.itemSize = 3;
 	triangleVertexColorBuffer.numItems = colors.length / 3;
 
-	// Associating to the vertex shader
-
-	gl.vertexAttribPointer(shaderProgram.vertexColorAttribute,
-	triangleVertexColorBuffer.itemSize,
-	gl.FLOAT, false, 0, 0);
-
+	gl.vertexAttribPointer( shaderProgram.vertexColorAttribute,
+							triangleVertexColorBuffer.itemSize,
+							gl.FLOAT, false, 0, 0);
 }
 
-//----------------------------------------------------------------------------
-
-
-function switchBars(b1, b2){
-	for (var i = 1; i < 36*3; i+=3){
-		if (vertices[b1*36*3+i] != -0.8){
-			var temp = vertices[b1*36*3 +i];
-			vertices[b1*36*3+i] = vertices[b2*36*3+i];
-			vertices[b2*36*3+i] = temp;
-		}
-	}
-}
-
-//  Drawing the model
-
-function drawModel( angleXX, angleYY, angleZZ,
-					sx, sy, sz,
-					tx, ty, tz,
-					mvMatrix,
-					primitiveType ) {
-
-    // Pay attention to transformation order !!
-
-
+function drawModel( angleXX, angleYY, angleZZ, sx, sy, sz, tx, ty, tz, mvMatrix){
 	mvMatrix = mult( mvMatrix, translationMatrix( tx, ty, tz ) );
-
 	mvMatrix = mult( mvMatrix, rotationZZMatrix( angleZZ ) );
-
 	mvMatrix = mult( mvMatrix, rotationYYMatrix( angleYY ) );
-
 	mvMatrix = mult( mvMatrix, rotationXXMatrix( angleXX ) );
-
 	mvMatrix = mult( mvMatrix, scalingMatrix( sx, sy, sz ) );
 
-	// Passing the Model View Matrix to apply the current transformation
-
 	var mvUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
-
 	gl.uniformMatrix4fv(mvUniform, false, new Float32Array(flatten(mvMatrix)));
-
-	// Drawing the contents of the vertex buffer
-
-	// primitiveType allows drawing as filled triangles / wireframe / vertices
-
-	if( primitiveType == gl.LINE_LOOP ) {
-
-		// To simulate wireframe drawing!
-
-		// No faces are defined! There are no hidden lines!
-
-		// Taking the vertices 3 by 3 and drawing a LINE_LOOP
-
-		var i;
-
-		for( i = 0; i < triangleVertexPositionBuffer.numItems / 3; i++ ) {
-
-			gl.drawArrays( primitiveType, 3 * i, 3 );
-		}
-	}
-	else {
-
-		gl.drawArrays(primitiveType, 0, triangleVertexPositionBuffer.numItems);
-
-	}
+	gl.drawArrays(gl.TRIANGLES, 0, triangleVertexPositionBuffer.numItems);
 }
-
-//----------------------------------------------------------------------------
-
-//  Drawing the 3D scene
 
 function drawScene() {
-
 	var pMatrix;
-
 	var mvMatrix = mat4();
 
-	// Clearing with the background color
-
 	gl.clear(gl.COLOR_BUFFER_BIT);
-
-	// NEW --- Computing the Projection Matrix
-
-	if( projectionType == 0 ) {
-
-		// For now, the default orthogonal view volume
-
-		pMatrix = ortho( -1.0, 1.0, -1.0, 1.0, -1.0, 1.0 );
-
-		tz = 0;
-
-		// TO BE DONE !
-
-		// Allow the user to control the size of the view volume
-	}
-	else {
-
-		// A standard view volume.
-
-		// Viewer is at (0,0,0)
-
-		// Ensure that the model is "inside" the view volume
-
-		pMatrix = perspective( 45, 1, 0.05, 10 );
-
-		tz = -2.25;
-
-	}
-
-	// Passing the Projection Matrix to apply the current projection
+	pMatrix = perspective( 45, 1, 0.05, 10 );
+	tz = -2.25;
 
 	var pUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
-
 	gl.uniformMatrix4fv(pUniform, false, new Float32Array(flatten(pMatrix)));
 
-	// NEW --- Instantianting the same model more than once !!
-
-	// And with diferent transformation parameters !!
-
-	// Call the drawModel function !!
-
-	// Instance 1 --- RIGHT TOP
-
-	drawModel( -angleXX, angleYY, angleZZ,
-	           sx, sy, sz,
-	           tx, ty, tz,
-	           mvMatrix,
-	           primitiveType );
-
+	drawModel(-angleXX, angleYY, angleZZ, sx, sy, sz, tx, ty, tz, mvMatrix);
 }
 
-//----------------------------------------------------------------------------
-//
-//  NEW --- Animation
-//
+function initWebGL( canvas ) {
+	try {
+		gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+		gl.enable( gl.DEPTH_TEST );
+	} catch (e) {}
 
-// Animation --- Updating transformation parameters
+	if (!gl) {
+		alert("Could not initialise WebGL, sorry! :-(");
+	}
+}
 
-var lastTime = 0;
+function runWebGL() {
+	var canvas = document.getElementById("my-canvas");
+	initWebGL(canvas);
+
+	triangleVertexPositionBuffer = gl.createBuffer();
+	triangleVertexColorBuffer = gl.createBuffer();
+	shaderProgram = initShaders(gl);
+	setEventListeners(canvas);
+
+	createSortedBars();
+	initArray = randomArray.slice();
+	initVertex = vertices.slice();
+	setRandomBars(randomArray);
+
+	tick();
+}
 
 function animate() {
-
 	var timeNow = new Date().getTime();
-
 	if( lastTime != 0 ) {
-
 		var elapsed = timeNow - lastTime;
-
 		if( rotationXX_ON ) {
-
 			angleXX += rotationXX_DIR * rotationXX_SPEED * (90 * elapsed) / 1000.0;
 	    }
-
 		if( rotationYY_ON ) {
-
 			angleYY += rotationYY_DIR * rotationYY_SPEED * (90 * elapsed) / 1000.0;
 	    }
-
 		if( rotationZZ_ON ) {
-
 			angleZZ += rotationZZ_DIR * rotationZZ_SPEED * (90 * elapsed) / 1000.0;
 	    }
 	}
-
 	lastTime = timeNow;
 }
 
-//----------------------------------------------------------------------------
-
-// Handling mouse events
-
-// Adapted from www.learningwebgl.com
-
-
-var mouseDown = false;
-
-var lastMouseX = null;
-
-var lastMouseY = null;
-
 function handleMouseDown(event) {
-
     mouseDown = true;
-
     lastMouseX = event.clientX;
-
     lastMouseY = event.clientY;
 }
 
 function handleMouseUp(event) {
-
     mouseDown = false;
 }
 
 function handleMouseMove(event) {
-
     if (!mouseDown) {
-
       return;
     }
-
-    // Rotation angles proportional to cursor displacement
-
     var newX = event.clientX;
-
     var newY = event.clientY;
-
     var deltaX = newX - lastMouseX;
-
     angleYY += radians( 10 * deltaX  )
-
     var deltaY = newY - lastMouseY;
-
     angleXX += radians( 10 * deltaY  )
-
     lastMouseX = newX
-
     lastMouseY = newY;
   }
-//----------------------------------------------------------------------------
-
-// Timer
-
 function tick() {
-
 	changeBlue(bluePos);
-
 	requestAnimFrame(tick);
 
-	//switchBars(0, 1);
-
 	initBuffers();
-
-	// NEW --- Processing keyboard events
-
-
 	drawScene();
-
-	//animate();
 }
 
-//----------------------------------------------------------------------------
-
-var intervalID;
-var index;
 function setEventListeners( canvas ){
-
-	// NEW ---Handling the mouse
-
-	// From learningwebgl.com
-
     canvas.onmousedown = handleMouseDown;
-
     document.onmouseup = handleMouseUp;
-
     document.onmousemove = handleMouseMove;
 
-
 	document.getElementById("reset-button").onclick = function(){
-
 		reset();
 	};
 
 	document.getElementById("start").onclick = function(){
 		ongoing = true;
-		var s = document.getElementById("sort-select");
-
-		var sel = s.selectedIndex;
-
-		switch(sel){
-					case 0 :
-						// Bubble Sort
-						index=0;
-						bluePos = 0;
-						intervalID = setInterval(sortArrayBubble, delay);
-						break;
-					case 1 :
-						// Selection Sort
-						index=0;
-						aux = 0;
-						bluePos = 0;
-						min = randomArray[index];
-						intervalID = setInterval(sortArraySelection, delay);
-						break;
-					case 2 :
-						// Insertion Sort
-						index=1;
-						aux = 1;
-						bluePos = 1;
-						intervalID = setInterval(sortArrayInsertion, delay);
-						break;
+		var s = document.getElementById("sort-select").selectedIndex;
+		switch(s){
+			case 0 :
+				// Bubble Sort
+				if(!paused){
+					index=0;
+					bluePos = 0;
 				}
+				intervalID = setInterval(sortArrayBubble, delay);
+				break;
+			case 1 :
+				// Selection Sort
+				if(!paused){
+					index=0;
+					aux = 0;
+					bluePos = 0;
+					min = randomArray[index];
+				}
+				intervalID = setInterval(sortArraySelection, delay);
+				break;
+			case 2 :
+				// Insertion Sort
+				if(!paused){
+					index=1;
+					aux = 1;
+					bluePos = 1;
+				}
+				intervalID = setInterval(sortArrayInsertion, delay);
+				break;
+		}
+		paused = false;
 	};
 
 	document.getElementById("pause").onclick = function(){
 		clearInterval(intervalID);
 		ongoing = false;
-		var t = bluePos;
-		bluePos = -1;
-		changeColors(t);
+		paused = true;
 	};
 
 	document.getElementById("sort-select").onchange = function(){
@@ -444,7 +243,6 @@ function setEventListeners( canvas ){
 	document.getElementById("myRange").oninput = function(){
 		delay = document.getElementById("myRange").value;
 		document.getElementById("delay").innerHTML = delay;
-
 		if(ongoing){
 			var s = document.getElementById("sort-select").selectedIndex;
 			clearInterval(intervalID);
@@ -469,34 +267,34 @@ function setEventListeners( canvas ){
 function sortArrayBubble() {
 	var swapped;
 	swapped = false;
-	for (i=index; i < randomArray.length; i++) {
-		bluePos = i+1;
-		if (randomArray[i] > randomArray[i+1]) {
-			var temp = randomArray[i];
-			randomArray[i] = randomArray[i+1];
-			randomArray[i+1] = temp;
-			swapped = true;
-			switchBars(i, i+1);
-		}
-		index+=1;
-		if(index == randomArray.length){
-			index = 0;
-		}
-		break;
+	bluePos = index+1;
+	if(randomArray[index] > randomArray[index+1]){
+		var temp = randomArray[index];
+		randomArray[index] = randomArray[index+1];
+		randomArray[index+1] = temp;
+		swapped = true;
+		switchBars(index, index+1);
+		swaps +=1;
 	}
-	changeColors(i);
-	changeColors(i+1);
+	index+=1;
+	if(index == randomArray.length){
+		changeColors(index-1);
+		index = 0;
+	}
+	changeColors(index-1);
+	changeColors(index);
+	changeColors(index+1);
 	if (isFinished()){
 		clearInterval(intervalID);
 	}
+	iterations +=1;
 	updateStats();
 }
 
 function sortArraySelection() {
-	var i = aux+1;
-	bluePos = i;
-	if (randomArray[min] > randomArray[i]) {
-		min = i;
+	bluePos = aux+1;
+	if (randomArray[min] > randomArray[aux+1]) {
+		min = aux+1;
 	}
 	aux+=1;
 	if(aux == randomArray.length){
@@ -509,11 +307,13 @@ function sortArraySelection() {
 		index+=1;
 		aux = index;
 		min = aux;
+		swaps +=1;
 	}
 	changeColors(bluePos-1);
 	if (isFinished()){
 		clearInterval(intervalID);
 	}
+	iterations +=1;
 	updateStats();
 }
 
@@ -529,6 +329,7 @@ function sortArrayInsertion() {
 		changeColors(aux);
 		changeColors(aux-1);
 		aux -=1;
+		swaps +=1;
 	}
 	else{
 		index+=1;
@@ -540,6 +341,7 @@ function sortArrayInsertion() {
 	if (isFinished()){
 		clearInterval(intervalID);
 	}
+	iterations +=1;
 	updateStats();
 }
 
@@ -552,77 +354,21 @@ function isFinished(){
 	return false;
 }
 
-//----------------------------------------------------------------------------
-//
-// WebGL Initialization
-//
-
-function initWebGL( canvas ) {
-	try {
-
-		// Create the WebGL context
-
-		// Some browsers still need "experimental-webgl"
-
-		gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-
-		// DEFAULT: The viewport occupies the whole canvas
-
-		// DEFAULT: The viewport background color is WHITE
-
-		// NEW - Drawing the triangles defining the model
-
-		primitiveType = gl.TRIANGLES;
-
-		// DEFAULT: The Depth-Buffer is DISABLED
-
-		// Enable it !
-
-		gl.enable( gl.DEPTH_TEST );
-
-	} catch (e) {
+function switchBars(b1, b2){
+	for (var i = 1; i < vertexPerCollumn; i+=3){
+		if (vertices[b1*vertexPerCollumn+i] != -0.8){
+			var temp = vertices[b1*vertexPerCollumn+i];
+			vertices[b1*vertexPerCollumn+i] = vertices[b2*vertexPerCollumn+i];
+			vertices[b2*vertexPerCollumn+i] = temp;
+		}
 	}
-	if (!gl) {
-		alert("Could not initialise WebGL, sorry! :-(");
-	}
-}
-
-//----------------------------------------------------------------------------
-
-function runWebGL() {
-
-	var canvas = document.getElementById("my-canvas");
-
-	projectionType = 1;
-
-	initWebGL( canvas );
-
-	triangleVertexPositionBuffer = gl.createBuffer();
-
-	triangleVertexColorBuffer = gl.createBuffer();
-
-
-	shaderProgram = initShaders( gl );
-
-	setEventListeners( canvas );
-
-
-	createSortedBars();
-
-	initArray = randomArray.slice();
-
-	initVertex = vertices.slice();
-
-	setRandomBars(randomArray);
-
-	tick();		// A timer controls the rendering / animation
 }
 
 function createSortedBars(){
-	var tmpColors = colors.slice(0, 36*3);
-	for (var i = 1; i < 19; i++){
-		var tmp = vertices.slice(0, 36*3);
-		for (var j = 1; j < 36*3; j+=3){
+	var tmpColors = colors.slice(0, vertexPerCollumn);
+	for (var i = 1; i < randomArray.length; i++){
+		var tmp = vertices.slice(0, vertexPerCollumn);
+		for (var j = 1; j < vertexPerCollumn; j+=3){
 			if (tmp[j] != -0.8){
 				tmp[j] = tmp[j] + 0.08*i;
 			}
@@ -630,33 +376,30 @@ function createSortedBars(){
 		}
 		vertices = vertices.concat(tmp);
 		colors = colors.concat(tmpColors);
-
 	}
 	sortedVertices = vertices.slice();
 }
 
 function setRandomBars(randomArray){
-	for (var i = 0; i < 19; i++){
+	for (var i = 0; i < randomArray.length; i++){
 		setBars(i, randomArray[i]);
 		changeColors(i);
 	}
-
 	initBuffers();
-
 	drawScene();
 }
 
 function setBars(b1, b2){
-	for (var i = 1; i < 36*3; i+=3){
-		if (vertices[b1*36*3+i] != -0.8){
-			vertices[b1*36*3+i] = sortedVertices[b2*36*3+i];
+	for (var i = 1; i < vertexPerCollumn; i+=3){
+		if (vertices[b1*vertexPerCollumn+i] != -0.8){
+			vertices[b1*vertexPerCollumn+i] = sortedVertices[b2*vertexPerCollumn+i];
 		}
 	}
 }
 
 function changeColors(pos){
-	var a = vertices.slice(pos*36*3, pos*36*3 + 36*3);
-	var b = sortedVertices.slice(pos*36*3, pos*36*3 + 36*3);
+	var a = vertices.slice(pos*vertexPerCollumn, pos*vertexPerCollumn + vertexPerCollumn);
+	var b = sortedVertices.slice(pos*vertexPerCollumn, pos*vertexPerCollumn + vertexPerCollumn);
 	if(ArrayEqual(a, b)){
 		changeGreen(pos);
 	}else{
@@ -665,20 +408,20 @@ function changeColors(pos){
 }
 
 function changeGreen(pos){
-	for(var i = 0; i < 36*3; i++){
-		colors[pos*36*3+i] = colorsGreen[i];
+	for(var i = 0; i < vertexPerCollumn; i++){
+		colors[pos*vertexPerCollumn+i] = colorsGreen[i];
 	}
 }
 
 function changeRed(pos){
-	for(var i = 0; i < 36*3; i++){
-		colors[pos*36*3+i] = colorsRed[i];
+	for(var i = 0; i < vertexPerCollumn; i++){
+		colors[pos*vertexPerCollumn+i] = colorsRed[i];
 	}
 }
 
 function changeBlue(pos){
-	for(var i = 0; i < 36*3; i++){
-		colors[pos*36*3+i] = colorsBlue[i];
+	for(var i = 0; i < vertexPerCollumn; i++){
+		colors[pos*vertexPerCollumn+i] = colorsBlue[i];
 	}
 }
 
@@ -695,65 +438,42 @@ function ArrayEqual(a, b){
 }
 
 function reset(){
-	// The initial values
-
 		tx = 0.0;
-
 		ty = 0.0;
-
 		tz = 0.0;
-
 		angleXX = 0.0;
-
 		angleYY = 0.0;
-
 		angleZZ = 0.0;
-
 		sx = 0.9;
-
 		sy = 0.9;
-
 		sz = 0.9;
-
 		rotationXX_ON = 0;
-
 		rotationXX_DIR = 1;
-
 		rotationXX_SPEED = 1;
-
 		rotationYY_ON = 0;
-
 		rotationYY_DIR = 1;
-
 		rotationYY_SPEED = 1;
-
 		rotationZZ_ON = 0;
-
 		rotationZZ_DIR = 1;
-
 		rotationZZ_SPEED = 1;
 
 		clearInterval(intervalID);
-
 		resetStats();
-
 		bluePos = -1;
-
 		ongoing = false;
-
+		paused = false;
 		randomArray = initArray.slice();
-
 		setRandomBars(randomArray);
 }
 
 function updateStats(){
-	iterations +=1;
 	document.getElementById("iterations").innerHTML = iterations;
+	document.getElementById("swaps").innerHTML = swaps;
 }
 
 function resetStats(){
 	iterations = 0;
+	swaps = 0;
 	document.getElementById("iterations").innerHTML = iterations;
-
+	document.getElementById("swaps").innerHTML = swaps;
 }
-
